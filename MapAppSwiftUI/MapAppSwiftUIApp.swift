18d97fe4ac15222ed1b43dd958387a2339f22a63
Umut Ağbaba <agbabau@gmail.com>
2023-04-08 11:12:14 +0300

    Initial Commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by Umut Ağbaba on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
