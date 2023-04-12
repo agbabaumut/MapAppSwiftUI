390be52cc0b266dd65036416d6cf0e1c9962bacd
Umut Ağbaba <agbabau@gmail.com>
2023-04-12 13:13:59 +0300

    done major features
    I have finished most of features and functionalities

--- MapAppSwiftUI/ViewModels/LocationsViewModel.swift
+++ MapAppSwiftUI/ViewModels/LocationsViewModel.swift
@@ -21,11 +21,14 @@ class LocationsViewModel: ObservableObject {
     
     // current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
-    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+    let miniMapSpan = MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008)
     
     // show list of locations
     @Published var showLocationsList: Bool = false
     
+    // show location detail via sheet
+    @Published var sheetLocation: Location? = nil
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
@@ -52,6 +55,32 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
             
         }
+    }
+    
+    func nextButtonPressed() {
+            // Get the current index
+            guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+                print("Could not find current index in locations array! Should never happen.")
+                return
+            }
+            
+            // Check if the currentIndex is valid
+            let nextIndex = currentIndex + 1
+            guard locations.indices.contains(nextIndex) else {
+                // Next index is NOT valid
+                // Restart from 0
+                guard let firstLocation = locations.first else { return }
+                showNextLocation(location: firstLocation)
+                return
+            }
+            
+            // Next index IS valid
+            let nextLocation = locations[nextIndex]
+            showNextLocation(location: nextLocation)
+        }
+    
+    func learnMoreButtonTapped() {
+        
     }
     
 }
--- MapAppSwiftUI/Views/LocationsView.swift
+++ MapAppSwiftUI/Views/LocationsView.swift
@@ -14,17 +14,17 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
             VStack(spacing: 0) {
                 header
                     .padding()
                 Spacer()
-                
-                
+                locationsPreviewStack
             }
-            
-            
+        }
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
         }
     }
 }
@@ -57,10 +57,37 @@ extension LocationsView {
             if vm.showLocationsList {
                 LocationsListView()
             }
-            
         }
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1.0 : 0.7)
+                    .shadow(radius: 10.0)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3),radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
